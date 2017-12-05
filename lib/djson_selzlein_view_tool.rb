ce7a0d137695a410714a05f53618e84db52597f9
Djeison Selzlein <djeisonaguir@gmail.com>
2017-12-04 22:30:14 -0200

    View Tool initial commit

---
+++ lib/djson_selzlein_view_tool.rb
@@ -0,0 +1,6 @@
+require "djson_selzlein_view_tool/version"
+require "djson_selzlein_view_tool/renderer"
+
+module DjsonSelzleinViewTool
+  # Your code goes here...
+end
---
+++ lib/djson_selzlein_view_tool/version.rb
@@ -0,0 +1,3 @@
+module DjsonSelzleinViewTool
+  VERSION = "0.1.0"
+end
